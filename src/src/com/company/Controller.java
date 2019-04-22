019c13bc315d0e21f20a1b756f4b5758775a5c36
corneliusyan <onel.raja@gmail.com>
2019-04-22 17:01:50 +0700

    Add messenger and texteditor in controller

--- src/src/com/company/Controller.java
+++ src/src/com/company/Controller.java
@@ -1,14 +1,29 @@
 package com.company;
 
-public class Controller {
+public class Controller implements TextEditorListener {
 
     private CRDT crdt;
     private String siteId;
+    private TextEditor textEditor;
+    private Messenger messenger;
 
     public Controller() {
         siteId = "ci papi";
         crdt = new CRDT(siteId);
+        textEditor = new TextEditor();
+        messenger = new Messenger();
     }
+
+    @Override
+    public void onInsert(char value, int index) {
+        crdt.localInsert(value, index);
+    }
+
+    @Override
+    public void onDelete(int index) {
+        crdt.localDelete(index);
+    }
+
 
     public void start() {
         for (int i = 0; i < 100; i++) {
