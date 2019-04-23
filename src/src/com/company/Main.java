80ede889735c2514f0062f257e77b49ce93330ed
Ahmad Izzan <aahmadizzan@gmail.com>
2019-04-24 06:32:40 +0700

    Add input reader for dynamic host and peers configuration

--- src/src/com/company/Main.java
+++ src/src/com/company/Main.java
@@ -1,14 +1,31 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Program Start");
+        Scanner in = new Scanner(System.in);
 
-        String host = "localhost";
-        int port = 9001;
+        System.out.println("================================");
+        System.out.print("Input host = ");
+        String host = in.nextLine();
+        System.out.print("Input port = ");
+        int port = in.nextInt();
 
-        Controller controller = new Controller(host, port);
+        System.out.println("--------------------------------");
+        List<String> peers = new ArrayList<String>();
+        System.out.print("Enter number of peers = ");
+        int nPeers = in.nextInt();
+        String buf = in.nextLine(); // consume \n after nextInt
+        for (int i = 1; i <= nPeers; i++) {
+            System.out.print("> Peer " + i + " URI = ");
+            String peer = in.nextLine();
+            peers.add(peer);
+        }
+
+        Controller controller = new Controller(host, port, peers);
         controller.start();
-
     }
 }
